ce402d63a476011160caf2dd1ac7c526e1a8d3e1
Vsevolod Zoria <vsevolodzoria2@gmail.com>
2024-08-29 22:07:13 +0300

    swc-loader

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,13 @@ import { App } from './app';
 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Root element not found');
+}
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>
